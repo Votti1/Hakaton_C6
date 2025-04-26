02094f678212ed7bbf1d8642602929d9893375fb
Sn0vvl4nd3R <MultiBillions10kk@gmail.com>
2025-04-26 13:43:12 +0800

    Quick fix version 3...

--- network.cpp
+++ network.cpp
@@ -3,7 +3,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
-void setupWiFi() {
+void SetupWiFi() {
   WiFi.begin(wifi_ssid, wifi_pass);
   Serial.print(F("Connecting to Wi-Fi"));
   uint8_t wifi_tries = 0;
@@ -22,7 +22,7 @@ void setupWiFi() {
   }
 }
 
-void sendDataToThingSpeak(float t_water, float t_oil, float fan_rpm, float pump_lpm) {
+void SendDataToThingSpeak(float t_water, float t_oil, float fan_rpm, float pump_lpm) {
   if (WiFi.status() != WL_CONNECTED) {
     Serial.println(F("WiFi disconnected. Cannot send data."));
     return;
